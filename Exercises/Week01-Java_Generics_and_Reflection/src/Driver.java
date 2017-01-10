21eca3537df7dc0555d12beb902a2101413be8fb
nathanhanak <nathan.hanak@gmail.com>
2017-01-10 16:51:37 +0000

    Adding folders now containing exercise modules

---
+++ Exercises/Week01-Java_Generics_and_Reflection/src/Driver.java
@@ -0,0 +1,36 @@
+public class Driver {
+
+    public static void main(String[] args) {
+
+        Storage<BankAccount> aStorage = new Storage<>();
+        Storage<String> sStorage = new Storage<>();
+
+        //Class baCls = BankAccount.class;
+        Class<BankAccount> baCls = BankAccount.class;
+
+        try {
+            //Object myAccount = baCls.newInstance();
+            BankAccount myAccount = baCls.newInstance();
+            //BankAccount myAccount = (BankAccount) baCls.newInstance(); - need to do if you run 'Class baCls = BankAccount.class;'
+
+            aStorage.setValue(myAccount);
+// Deposit
+            myAccount.deposit(15);
+        }
+        catch ( InstantiationException e ) {
+            e.printStackTrace();
+        }
+        catch ( IllegalAccessException e ) {
+            e.printStackTrace();
+        }
+
+        System.out.println( aStorage.getValue().showBalance() );
+        if( aStorage.getClass() == sStorage.getClass() ) {
+            System.out.println( "EQUAL" );
+        } else {
+            System.out.println( "NOT EQUAL" );
+        }
+
+    }
+
+}
