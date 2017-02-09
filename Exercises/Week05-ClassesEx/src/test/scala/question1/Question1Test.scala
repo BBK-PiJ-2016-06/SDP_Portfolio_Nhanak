34b5497eb8edc0b4e47369d4a1b9ac01814446b7
nathanhanak <nathan.hanak@gmail.com>
2017-02-09 20:58:18 +0000

    Implemented question 1's tests, backwards, I know

--- Exercises/Week05-ClassesEx/src/test/scala/question1/Question1Test.scala
+++ Exercises/Week05-ClassesEx/src/test/scala/question1/Question1Test.scala
@@ -23,6 +23,24 @@ class Question1Test extends FunSuite {
     assert(new Counter(5).dec().count == 4)
   }
 
+  test("For CounterAsCase Calling inc with a parameter increases as expected") {
+    assert(new CounterAsCase(4).inc(3).count == 7)
+  }
 
+  test("For CounterAsCase Calling dec with a parameter increases as expected") {
+    assert(new CounterAsCase(4).dec(2).count == 2)
+  }
 
+  test("For CounterAsCase Calling inc with no parameter increases count by 1 ") {
+    assert(new CounterAsCase(5).inc().count == 6)
+  }
+
+  test("For CounterAsCase Calling dec with no parameter decreases count by 1") {
+    assert(new CounterAsCase(5).dec().count == 4)
+  }
+
+  test("Calling adjust with an int adds the int to count's total") {
+    assert(new Counter(5).adjust(new Adder(3)).count == 8)
+  }
+  
 }
