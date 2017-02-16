1c5968f8eea45f58ddeb6dbcd15bff6a55efbd90
nateHan <nhanak01@dcs.bbk.uk>
2017-02-16 18:44:28 +0000

    Created and completed MulInstruction and MulInstruction tests

---
+++ Coursework/cw-one/src/main/scala/sml/MulInstruction.scala
@@ -0,0 +1,24 @@
+package sml
+
+/**
+  * Created by nathanhanak on 2/16/17.
+  */
+class MulInstruction(label: String, op: String, val result: Int, val op1: Int, val op2: Int)
+  extends Instruction(label, op) {
+
+  override def execute(m: Machine) = {
+    val value1 = m.regs(op1)
+    val value2 = m.regs(op2)
+    m.regs(result) = value1 * value2
+  }
+
+  override def toString(): String =
+    super.toString() + s" will store result of op1:$op1 * op2:$op2 in result register:$result"
+
+}
+
+object MulInstruction {
+
+  def apply(label: String, result: Int, op1: Int, op2: Int) =
+    new MulInstruction(label, "mul", result, op1, op2)
+}
---
+++ Coursework/cw-one/src/test/scala/sml/MulInstructionTest.scala
@@ -0,0 +1,67 @@
+package sml
+
+import org.scalatest.FunSpec
+import org.scalatest.GivenWhenThen
+
+/**
+  * Created by nathanhanak on 2/16/17.
+  */
+class MulInstructionTest extends FunSpec with GivenWhenThen {
+
+  var mulInst: MulInstruction = _
+  val m : Machine = new Machine(Labels(), Vector())
+  val label = "In1"
+  val opCode = "mul"
+  val resultRegister = 3
+  m.regs(4) = 10
+  m.regs(5) = 4
+
+  describe("A MulInstruction") {
+
+    it("should print off its label, op instruction, result register, and two operands") {
+      Given("A new MulInstruction")
+      mulInst = new MulInstruction(label, opCode, resultRegister, 6, 2)
+
+      When( "calling toString()")
+      assert(mulInst.toString().equals(s"$label: $opCode will store result of op1:6 * op2:2 in result register:$resultRegister"))
+
+    }
+
+    it("should store the result of the operation in the given machine") {
+      Given("A new MulInstruction")
+      mulInst = new MulInstruction(label, opCode, resultRegister, 4, 5)
+
+      When("calling execute(Machine), the Machine will store the result in the expected register")
+      mulInst.execute(m)
+      assert(m.regs(resultRegister) == 40)
+    }
+
+    it("should create a new instance of a mulInstruction object") {
+      Given("a MulInstruction object instantiated from its singleton object's apply method")
+      val sI : MulInstruction = MulInstruction.apply(label, resultRegister, 4, 5)
+
+      When("calling toString(), it prints as expected.")
+      assert(sI.toString().equals(s"$label: $opCode will store result of op1:4 * op2:5 in result register:$resultRegister"))
+
+      Then("calling execute(Machine) from that object will store the result in the machine as expected")
+      sI.execute(m)
+      assert(m.regs(resultRegister) == 40)
+    }
+
+    it("should throw an exception if the result, op1, or op2 are out of the bounds of the machine's regs") {
+      Given("an array of MulInstructions with a negative or overly large results, op1 or op2's")
+      val mulInstArray: Array[MulInstruction] = Array(MulInstruction.apply(label, -3, 4, 5), MulInstruction.apply(label, 33, 4, 5),
+        MulInstruction.apply(label, 3, 44, 5), MulInstruction.apply(label, 3, 4, -5), MulInstruction.apply(label, 3, 4, 55) )
+
+
+      When("calling execute with out of bounds fields, it throws IndexOutOfBoundsExceptions")
+      for (i <- mulInstArray.indices)
+        intercept[IndexOutOfBoundsException] {
+          mulInstArray(i).execute(m)
+        }
+    }
+
+
+  }
+
+}
