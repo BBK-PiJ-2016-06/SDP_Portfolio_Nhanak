c3f0338a430c98e78f8363200518b129654e91a0
nateHan <nhanak01@dcs.bbk.uk>
2017-02-23 22:12:52 +0000

    Created eager singleton object

--- Exercises/Week07-Exercises/src/main/scala/singleton/EagerShield.scala
+++ Exercises/Week07-Exercises/src/main/scala/singleton/EagerShield.scala
@@ -3,6 +3,30 @@ package singleton
 /**
   * Created by nathanhanak on 2/23/17.
   */
-class EagerShield {
+class EagerShield private() {
+
+  private var protecting : Boolean = false
+
+  def raiseShield(epg : PowerGenerator) = if (epg.getPower() > 50) protecting = true
+
+  def lowerShield(epg : PowerGenerator): Unit  = {
+    protecting = false
+    epg.returnPower(50)
+  }
+
+  def getStatus(): Boolean = protecting
 
 }
+
+object EagerShield {
+ 
+  private val shield : EagerShield = new EagerShield()
+  
+  def getInstance(pg : PowerGenerator): EagerShield = {
+    pg.usePower(70)
+    shield
+  }
+
+}
+
+
