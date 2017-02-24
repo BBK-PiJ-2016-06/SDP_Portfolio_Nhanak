7cef3f00119ab1324058cc5bf5c2503b34d72f82
nateHan <nhanak01@dcs.bbk.uk>
2017-02-24 18:05:27 +0000

    had  to make CommentaryObject inherit Commentary to make asInstanceOf line work

--- Exercises/Week07-Exercises/src/main/scala/observer/CommentaryObject.scala
+++ Exercises/Week07-Exercises/src/main/scala/observer/CommentaryObject.scala
@@ -2,12 +2,30 @@ package observer
 
 import scala.collection.mutable.ListBuffer
 
-class CommentaryObject(var subscribers: ListBuffer[Observer], val title: String) extends Subject {
-  def subscribeObserver(observer: Observer) = ???
+class CommentaryObject(var subscribers: ListBuffer[Observer], val title: String) extends Subject with Commentary {
 
-  def unSubscribeObserver(observer: Observer) = ???
+  var latestUpdate = ""
 
-  def notifyObservers() = ???
+  override def subscribeObserver(observer: Observer): Unit = {
+    println(s"Subscribing new Observer: ${observer.hashCode()}")
+    subscribers += observer
+  }
 
-  def subjectDetails: String = ???
+  override def unSubscribeObserver(observer: Observer): Unit = {
+    println(s"Unsubscribing Observer: ${observer.hashCode()}")
+    subscribers -= observer
+  }
+
+  override def notifyObservers(): Unit = {
+    for ( observer <- subscribers) {
+      observer.update(subjectDetails)
+    }
+  }
+
+  override def subjectDetails: String = title + "\n" + latestUpdate
+
+  override def setDesc(update: String): Unit = {
+    latestUpdate = update
+    notifyObservers()
+  }
 }
--- Exercises/Week07-Exercises/src/main/scala/observer/SMSUsers.scala
+++ Exercises/Week07-Exercises/src/main/scala/observer/SMSUsers.scala
@@ -1,9 +1,18 @@
 package observer
 
 case class SMSUsers(s: Subject, msg: String) extends Observer {
-  def update(desc: String) = ???
 
-  def subscribe() = ???
+  var latestUpdate = "Welcome to Sports Lobby Live Update"
 
-  def unSubscribe() = ???
+  def update(desc: String):Unit = {
+    latestUpdate = desc
+    println(s"User ${this.hashCode()} received update: $desc")
+  }
+
+
+  def subscribe() = s.subscribeObserver(this)
+
+  def unSubscribe() = s.unSubscribeObserver(this)
+
+
 }
