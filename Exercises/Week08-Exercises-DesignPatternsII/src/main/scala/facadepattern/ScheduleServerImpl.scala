303260a9e0c89341a34927ddcb2faefbd0789f64
nateHan <nhanak01@dcs.bbk.uk>
2017-04-20 20:46:47 +0100

    Implemented Facade pattern classes

--- Exercises/Week08-Exercises-DesignPatternsII/src/main/scala/facadepattern/ScheduleServerImpl.scala
+++ Exercises/Week08-Exercises-DesignPatternsII/src/main/scala/facadepattern/ScheduleServerImpl.scala
@@ -1,29 +1,29 @@
 package facadepattern
 
-//TODO
 
 case class ScheduleServerImpl() extends ScheduleServer {
-  override def startBooting: Unit = ???
 
-  override def readSystemConfigFile: Unit = ???
+  override def startBooting: Unit = print("Starting system from scratch \n")
 
-  override def init: Unit = ???
+  override def readSystemConfigFile: Unit = print("Reading that SysConfigFile \n")
 
-  override def initializeContext: Unit = ???
+  override def init: Unit = print("Now initializing \n")
 
-  override def initializeListeners: Unit = ???
+  override def initializeContext: Unit = print("Starting context \n")
 
-  override def createSystemObjects: Unit = ???
+  override def initializeListeners: Unit = print("Gather round now listeners \n")
 
-  override def releaseProcesses: Unit = ???
+  override def createSystemObjects: Unit = print("Make all the objects minions \n")
 
-  override def destory: Unit = ???
+  override def releaseProcesses: Unit = print("Time for processes to go home \n")
 
-  override def destroySystemObjects: Unit = ???
+  override def destory: Unit = print("KILL SMASH DIE \n")
 
-  override def destoryListeners: Unit = ???
+  override def destroySystemObjects: Unit = print("Kill your darlings, your system object darlings \n")
 
-  override def destoryContext: Unit = ???
+  override def destoryListeners: Unit = print("Engage in secrecy coverup, kill listeners \n")
 
-  override def shutdown: Unit = ???
+  override def destoryContext: Unit = print("Confuse them all, eliminate context \n")
+
+  override def shutdown: Unit = print("Goodbye Dave \n")
 }
