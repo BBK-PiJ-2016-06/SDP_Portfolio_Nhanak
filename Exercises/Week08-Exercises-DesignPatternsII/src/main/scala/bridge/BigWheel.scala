161a431d28e67ed6ce905555bf435e51bb1b4c5f
nateHan <nhanak01@dcs.bbk.uk>
2017-04-21 12:34:58 +0100

    Wrote out composite word solutions

--- Exercises/Week08-Exercises-DesignPatternsII/src/main/scala/bridge/BigWheel.scala
+++ Exercises/Week08-Exercises-DesignPatternsII/src/main/scala/bridge/BigWheel.scala
@@ -1,15 +1,13 @@
 package bridge
 
-// TODO
-
 case class BigWheel(product: Product, s: String) extends Car(product, s) {
 
   override def assemble: Unit = println(s"Assembling product ${product.productName} for $s")
 
   override def produceProduct: Unit = {
-    product.produce
+    product.produce // this allows changes to future products, now you don't have to change BigWheel if that occurs.
     println(s"Modifying product ${product.productName} according to $s")
   }
 
-  override def printDetails: Unit = println(s"Car: $s  .   Product: ${product.productName}")
+  override def printDetails: Unit = println(s"Car: $s   Product: ${product.productName}")
 }
