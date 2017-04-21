2004c0cfe55ab57e45c8d182076aac56da3e3700
nateHan <nhanak01@dcs.bbk.uk>
2017-04-21 20:01:15 +0100

    Implemented Composite design pattern

--- Exercises/Week08-Exercises-DesignPatternsII/src/main/scala/composite/HtmlElement.scala
+++ Exercises/Week08-Exercises-DesignPatternsII/src/main/scala/composite/HtmlElement.scala
@@ -1,11 +1,18 @@
 package composite
 
-// TODO
+import java.util
 
 case class HtmlElement(s: String) extends HtmlTag(s) {
-  override def setStartTag(tag: String): Unit = ???
 
-  override def setEndTag(tag: String): Unit = ???
+  var startTag : String = new String()
+  var endTag : String = new String()
+  var htmlBody : String = new String()
+  val htmlChildren : util.List[HtmlTag] = new util.ArrayList()
 
-  override def generateHtml: Unit = ???
+
+  override def setStartTag(tag: String): Unit = startTag = tag
+
+  override def setEndTag(tag: String): Unit = endTag = tag
+
+  override def generateHtml: Unit = println(s"  $startTag$htmlBody$endTag")
 }
--- Exercises/Week08-Exercises-DesignPatternsII/src/main/scala/composite/HtmlTag.scala
+++ Exercises/Week08-Exercises-DesignPatternsII/src/main/scala/composite/HtmlTag.scala
@@ -3,19 +3,23 @@ package composite
 import java.util.List
 
 abstract class HtmlTag(tagName: String) {
+
+  var htmlBody : String
+  val htmlChildren : List[HtmlTag]
+
   def getTagName: String = tagName
 
   def setStartTag(tag: String)
 
   def setEndTag(tag: String)
 
-  def setTagBody(tagBody: String) = ???
+  def setTagBody(tagBody: String) = htmlBody = tagBody
 
-  def addChildTag(htmlTag: HtmlTag) = ???
+  def addChildTag(htmlTag: HtmlTag) = htmlChildren.add(htmlTag)
 
-  def removeChildTag(htmlTag: HtmlTag) = ???
+  def removeChildTag(htmlTag: HtmlTag) = htmlChildren.remove(htmlTag)
 
-  def getChildren: List[HtmlTag] = ???
+  def getChildren: List[HtmlTag] = htmlChildren
 
   def generateHtml
 }
